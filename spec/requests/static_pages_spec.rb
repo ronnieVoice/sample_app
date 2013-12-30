db321c472f26ad25e2e1fcfa56a654478cec08d4
Ronnie Brown <ronnievoice@gmail.com>
2013-12-29 19:05:02 -0800

    Make a basic User modelincluding secure passwords

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,30 +7,30 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('') }
-    it { should_not have_title('| Home')}
+    it { has_content?('Sample App') }
+    it { has_title?(full_title('')) }
+    it { has_title?('| Home')}
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it { has_content?('Help') }
+    it { has_title?(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us'))}
+    it { has_content?('About') }
+    it { has_title?(full_title('About Us')) }
   end
 
   describe "Contact" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { has_selector?('h1', text: 'Contact') }
+    it { has_title?(full_title('Contact')) }
   end
 
 end
